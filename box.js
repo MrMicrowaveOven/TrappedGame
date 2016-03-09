1c30e299d99f4eeef34271dc59b086be58e991f2
Benjamin <benj@zagorski.com>
2016-03-08 18:20:07 -0800

    AddedMath,GivingUpForNowDueToClickErrors

---
+++ box.js
@@ -0,0 +1,112 @@
+function Box(id, bottom) {
+
+  this.id = id;
+  if (this.id === undefined) {
+    this.id = 1;
+  }
+
+  this.boxStartX = 490;
+  this.boxStartY = -40;
+  this.heightOfBox = 60;
+  this.widthOfBox = 140;
+  this.fallSpeed = 2;
+  this.boxesQueue = [];
+
+  this.firstNum = Math.floor(Math.random() * 100);
+  this.secondNum = Math.floor(Math.random() * 100);
+  this.totalNum = this.firstNum + this.secondNum;
+
+  this.borderWidth = "2";
+
+  this.pathTop = -40;
+  this.pathBottom = bottom;
+  if (bottom === undefined) {
+    this.pathBottom = 260;
+  }
+  this.pathLength = this.pathBottom - this.pathTop;
+  this.doneMoving = false;
+
+  this.canvas = document.getElementById("myCanvas");
+  this.ctx = this.canvas.getContext("2d");
+
+}
+
+Box.prototype.drawBox = function(tlx, tly, brx, bry, color) {
+  this.ctx.beginPath();
+  this.ctx.rect(tlx, tly, brx, bry);
+  this.ctx.fillStyle = color;
+  this.ctx.fill();
+  this.ctx.closePath();
+
+// Set font
+  this.ctx.fillStyle = "black";
+  this.ctx.font = "20px Arial";
+  ctx.textAlign = "center";
+
+  var boxfill = this.firstNum + " + " + this.secondNum;
+  this.ctx.fillText(boxfill, this.boxStartX+this.widthOfBox/2, this.boxStartY+this.heightOfBox/2);
+
+  this.ctx.beginPath();
+  this.ctx.rect(tlx, tly, brx, bry);
+  this.ctx.lineWidth = this.borderWidth;
+  this.ctx.strokeStyle = "black";
+  this.ctx.stroke();
+  this.ctx.closePath();
+};
+
+Box.prototype.reduceSpeed = function(killKey) {
+
+    if (this.boxStartY === this.pathLength / 2) {
+      this.fallSpeed = this.fallSpeed/2;
+    }
+    // if (this.boxStartY === 2*this.pathLength / 3) {
+    //   this.fallSpeed = this.fallSpeed/2;
+    // }
+    if (this.boxStartY === this.pathLength * 3 / 4) {
+      this.fallSpeed = this.fallSpeed/2;
+    }
+    if (this.boxStartY === this.pathLength * 7 / 8) {
+      this.fallSpeed = this.fallSpeed/2;
+    }
+    if (this.boxStartY === this.pathLength) {
+      this.fallSpeed = 0;
+      this.doneMoving = true;
+      if (this.pathBottom > 60)
+      {
+        var box2 = new Box(this.id + 1, this.pathBottom - this.heightOfBox);
+        allBoxes.push(box2);
+        clearInterval(killKey);
+        dropOneBox(box2);
+      } else {
+          clearInterval(killKey);
+          // alert("GAME OVER");
+          // document.location.reload();
+      }
+    }
+};
+
+Box.prototype.dropBox = function (killKey) {
+  this.ctx.clearRect(
+    this.boxStartX - 1,
+    this.boxStartY - 3,
+    this.widthOfBox + 2,
+    this.heightOfBox
+  );
+  // this.drawBox(
+  //   this.boxStartX - this.borderWidth - 1,
+  //   this.boxStartY - this.borderWidth - 1,
+  //   this.widthOfBox + this.borderWidth + 1,
+  //   this.heightOfBox + this.borderWidth
+  // );
+
+  this.drawBox(this.boxStartX, this.boxStartY,
+    this.widthOfBox, this.heightOfBox, "red");
+  this.boxStartY += this.fallSpeed;
+  this.reduceSpeed(killKey);
+};
+
+Box.prototype.removeBox = function () {
+  var placed = allBoxes.shift;
+};
+
+// window.box = Box;
---
+++ grid.js
@@ -0,0 +1,43 @@
+function Grid() {
+  this.gridX = 5;
+  this.gridY = 5;
+  this.squares = [];
+  this.spaceBetween = 5;
+  this.ctx = canvas.getContext("2d");
+
+}
+Grid.prototype.drawSquare = function(tlx, tly, brx, bry, color) {
+    this.ctx.beginPath();
+    this.ctx.rect(tlx, tly, brx, bry);
+    this.ctx.fillStyle = color;
+    this.ctx.fill();
+    this.ctx.closePath();
+
+    this.ctx.beginPath();
+    this.ctx.rect(tlx-this.spaceBetween/2, tly-this.spaceBetween/2,
+      brx+this.spaceBetween, bry+this.spaceBetween);
+    this.ctx.lineWidth="5";
+    this.ctx.strokeStyle = "black";
+    this.ctx.stroke();
+    this.ctx.closePath();
+  };
+
+Grid.prototype.drawGrid = function(tlx, tly, brx, bry) {
+    var widthOfEach = (brx - tlx)/this.gridX - this.spaceBetween;
+    var heightOfEach = (bry - tly)/this.gridY - this.spaceBetween;
+    console.log(widthOfEach);
+    console.log(heightOfEach);
+    var thisSquareX = tlx;
+    var thisSquareY = tly;
+    for (var i = 0; i < this.gridX; i++) {
+      for (var j = 0; j < this.gridY; j++) {
+        this.drawSquare(thisSquareX, thisSquareY,
+          widthOfEach, heightOfEach, "#0095DD");
+        this.squares.push([thisSquareX, thisSquareY,
+          widthOfEach, heightOfEach, [i,j]]);
+        thisSquareY += heightOfEach + this.spaceBetween;
+      }
+      thisSquareX += widthOfEach + this.spaceBetween;
+      thisSquareY = tly;
+    }
+  };
